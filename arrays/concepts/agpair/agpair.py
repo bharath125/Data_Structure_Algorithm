98b3131750807e42c56b02227cb26fcf674b85be
Bharath Nandipati <113271121+bharath125@users.noreply.github.com>
2026-03-01 18:37:46 +0530

    Create agpair.py with code

---
+++ arrays/concepts/agpair/agpair.py
@@ -0,0 +1,26 @@
+## brute force solution
+string='acbagkagg'
+length=len(string)
+
+pair_count=0
+for i in range(length-1):
+  if (string[i]=='a'):
+    for j in range(i+1,length-1):
+      if (string[j]=='g'):
+        pair_count+=1
+print(pair_count)
+
+
+## optimized solution TC:O(n) SC:O(1)
+string='acbagkagg'
+length=len(string)
+
+pair_count=0
+count_a=0
+for i in range(length):
+    if (string[i]=='a'):
+        count_a+=1
+    elif (string[i]=='g'):
+        pair_count+=count_a
+print(pair_count)
+        
