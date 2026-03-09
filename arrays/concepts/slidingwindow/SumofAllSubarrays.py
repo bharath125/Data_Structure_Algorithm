51b4dce1a65b7046b3980d4a3600374a3058e9a5
Bharath Nandipati <113271121+bharath125@users.noreply.github.com>
2026-03-10 02:37:33 +0530

    Updated prefixsum optimization solution SumofAllSubarrays.py

--- arrays/concepts/slidingwindow/SumofAllSubarrays.py
+++ arrays/concepts/slidingwindow/SumofAllSubarrays.py
@@ -15,6 +15,29 @@ for i in range(n):
         total_sum+=subArraySum
 print(total_sum)
 
+## can we optime above to O(n2)
+
+## prefx_sum optimization
+## TC : O(n2) SC:O(n)
+prefix_sum=[]
+total=0
+for i in range(n):
+    total+=A[i]
+    prefix_sum.append(total)
+    
+print(pf_sum)
+total_sum=0
+for i in range(n):
+    for j in range(i,n):
+        subArraySum=0
+        if i==0:
+            subArraySum=prefix_sum[j]
+        else:
+            subArraySum=prefix_sum[j]-prefix_sum[i-1]
+        total_sum+=subArraySum
+print(total_sum)
+
+
 
 
 
