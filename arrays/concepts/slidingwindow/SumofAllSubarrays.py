a206cf238c3d9991baf6e5e5802d318f9886cde7
Bharath Nandipati <113271121+bharath125@users.noreply.github.com>
2026-03-10 02:37:07 +0530

    Updated brute force  solution SumofAllSubarrays.py

--- arrays/concepts/slidingwindow/SumofAllSubarrays.py
+++ arrays/concepts/slidingwindow/SumofAllSubarrays.py
@@ -1,1 +1,29 @@
+## sum of all subarrays
+
+## brute force solution
+A=[2,8,1,3]
+n=len(A)
+total_sum=0
+
+## TC : O(n3) and SC: O(1)
+
+for i in range(n):
+    for j in range(i,n):
+        subArraySum=0
+        for k in range(i,j+1):
+            subArraySum+=A[k]
+        total_sum+=subArraySum
+print(total_sum)
+
+
+
+
+
+
+
+
+
+
+
+
 
