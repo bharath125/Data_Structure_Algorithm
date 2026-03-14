e606bef5a9f7cd8e11d40e4ff17cc1ef52e28602
Bharath Nandipati <113271121+bharath125@users.noreply.github.com>
2026-03-14 16:42:02 +0530

    Update squareRoot.py

--- arrays/concepts/binarySearch/squareRoot.py
+++ arrays/concepts/binarySearch/squareRoot.py
@@ -1,1 +1,15 @@
-
+## TC : O(logN) SC : O(1)
+    def sqrt(self, A):
+        L=1
+        R=A
+        ans=L
+        if A==0:
+            return A
+        while L<=R:
+            mid=L+(R-L)//2
+            if mid*mid<=A:
+                ans=mid
+                L=mid + 1
+            else:
+                R=mid - 1
+        return ans
