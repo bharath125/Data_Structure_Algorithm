d139896bba784a4b9671ce0741f202d8a42386e2
Bharath Nandipati <113271121+bharath125@users.noreply.github.com>
2026-03-20 00:46:32 +0530

    Updated SortedInsertPosition.py

--- arrays/concepts/binarySearch/SortedInsertPosition.py
+++ arrays/concepts/binarySearch/SortedInsertPosition.py
@@ -1,1 +1,16 @@
 ## SortedInsertPosition
+ def searchInsert(self, A, B):
+        N=len(A)
+        L=0   # left
+        R=N-1 # right
+        while L<=R:
+            mid=L+(R-L)//2  ## middle value
+ 
+            if A[mid]==B:
+                return mid 
+            elif A[mid]<B:
+                L=mid+1
+            elif A[mid]>B:
+                R=mid-1
+        
+        return L
