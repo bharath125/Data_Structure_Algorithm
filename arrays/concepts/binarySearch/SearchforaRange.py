8c2e5b7e2492d72fc507c63404066ac5bafbdbb1
Bharath Nandipati <113271121+bharath125@users.noreply.github.com>
2026-03-21 02:02:09 +0530

    Update SearchforaRange.py

--- arrays/concepts/binarySearch/SearchforaRange.py
+++ arrays/concepts/binarySearch/SearchforaRange.py
@@ -1,1 +1,29 @@
+ def searchRange(self, A, B):
+        N=len(A)
+        L=0
+        R=N-1
 
+        first_occ=-1
+        while L<=R:
+            mid=L+(R-L)//2
+            if A[mid]==B:
+                first_occ=mid
+                R=mid-1
+            elif A[mid]<B:
+                L=mid+1
+            else:
+                R=mid-1
+        Lo=0
+        Ro=N-1
+        sec_occ=-1
+        while Lo<=Ro:
+            mid=Lo+(Ro-Lo)//2
+            if A[mid]==B:
+                sec_occ=mid
+                Lo=mid+1
+            elif A[mid]<B:
+                Lo=mid+1
+            else:
+                Ro=mid-1
+           
+        return [first_occ,sec_occ]
