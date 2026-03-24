ef66e51ef25dfa417648a76595c0f7e10b51cf4f
Bharath Nandipati <113271121+bharath125@users.noreply.github.com>
2026-03-25 00:13:15 +0530

    Updated LinkedList.py

--- LinkedList/LinkedList.py
+++ LinkedList/LinkedList.py
@@ -1,1 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#    def __init__(self, x):
+#        self.val = x
+#        self.next = None
 
+class Solution:
+    # @param A : head node of linked list
+    def solve(self, A):
+        values=[]
+        current=A
+
+        while current:
+            values.append(str(current.val))
+            current=current.next
+        
+        print(" ".join(values) + " ")
