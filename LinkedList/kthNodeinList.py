8436f8a71665a15fab86bc87c1a71bce32f40f8f
Bharath Nandipati <113271121+bharath125@users.noreply.github.com>
2026-03-25 00:37:12 +0530

    Updated kthNodeinList.py

--- LinkedList/kthNodeinList.py
+++ LinkedList/kthNodeinList.py
@@ -1,1 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#    def __init__(self, x):
+#        self.val = x
+#        self.next = None
 
+class Solution:
+    # @param A : head node of linked list
+    # @param B : integer
+    # @return an integer
+    def solve(self, A, B):
+
+        temp=A
+        for i in range(0,B):
+            if temp!=None:
+                temp=temp.next
+        if temp==None:
+            return -1
+        else:
+            return temp.val
