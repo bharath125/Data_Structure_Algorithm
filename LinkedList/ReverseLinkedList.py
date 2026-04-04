862c3d7022115938986e3cf08b9516bd89bc3976
Bharath Nandipati <113271121+bharath125@users.noreply.github.com>
2026-04-05 02:25:03 +0530

    Update ReverseLinkedList.py

--- LinkedList/ReverseLinkedList.py
+++ LinkedList/ReverseLinkedList.py
@@ -1,1 +1,21 @@
-## ReverseLinkedList
+# Definition for singly-linked list.
+# class ListNode:
+#	def __init__(self, x):
+#		self.val = x
+#		self.next = None
+
+class Solution:
+	# @param A : head node of linked list
+	# @return the head node in the linked list
+	def reverseList(self, A):
+        prev=None
+        curr=A
+        if curr.next==None:
+            return A
+        while(curr!=None):
+            nextNode=curr.next
+            curr.next=prev
+            prev=curr
+            curr=nextNode
+        A=prev
+        return A
